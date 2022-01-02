5bf59db32807295e1ab0f3f1bdefc23df6b78812
kirito <manhtruong@gmail.com>
2022-01-02 13:30:12 +0700

    sidebar PHP

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect('localhost','root','','datatable_db');
+if(mysqli_connect_errno()){
+    echo"database connection error!";
+    exit;
+}
+?>
---
+++ sidebar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div>
+    <!-- <input type="checkbox" id="check">
+    <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-times" id="cancel"></i>
+    </label> -->
+    <div class="sidebar">
+        <header>Trường</header>
+        <ul>
+            <li><a href="index.php"><i class="fas fa-qrcode"></i>Thành viên</a></li>
+            <li><a href="trangchu.php"><i class="fas fa-link"></i>Trang Chủ</a></li>
+            <li><a href="#"><i class="fas fa-stream"></i>Overvlew</a></li>
+            <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
+            <li><a href="#"><i class="fas fa-question-circle"></i>About</a></li>
+            <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
+            <li><a href="#"><i class="fas fa-envelope"></i>Contact</a></li>                    
+        </ul>
+       
+        
+    </div>
+    <div id="header">
+            <ul id="nav">
+                <li><a href="">Home</a></li>
+                <li><a href="">Band</a></li>
+                <li><a href="">Tour</a></li>
+                <li><a href="">Contact</a></li>
+                <li>
+                    <a href="">More</a>
+                    <ul class="subnav">
+                        <li><a href="">Merchandise</a></li>
+                        <li><a href="">Extras</a></li>
+                        <li><a href="">Media</a></li>
+                    </ul>
+                </li>
+            </ul>
+    </div>
+    <section>
+
+    </section>
+    
+</body>
+</html>
