3110940235c92abc51e88ed0e649f463b4f4c90c
kirito <manhtruong@gmail.com>
2022-04-19 18:58:53 +0700

    modifile

--- connection.php
+++ connection.php
@@ -1,7 +1,22 @@
 <?php
-$con=mysqli_connect('localhost','root','','datatable_db');
-if(mysqli_connect_errno()){
-    echo"database connection error!";
+$con = mysqli_connect('localhost', 'root', '', 'datatable_db');
+if (mysqli_connect_errno()) {
+    echo "database connection error!";
     exit;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
